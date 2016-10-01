b5f97221ff9d7414d8502a69b166d68a9ff3b6b7
Baljit Singh <basingh89@gmail.com>
2016-09-30 23:51:25 -0400

    Accessors for size

--- Sudoku/Sudoku.cs
+++ Sudoku/Sudoku.cs
@@ -4,6 +4,36 @@ namespace SudokuSolver
 {
 	public class Sudoku
 	{
-		
+		/// <summary>
+		/// Stores the rank of the <see cref="Sudoku"/>.
+		/// </summary>
+		private readonly uint rank;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Sudoku"/> class.
+		/// </summary>
+		/// <param name="rank">Rank</param>
+		public Sudoku (uint rank)
+		{
+			this.rank = rank;
+		}
+
+		/// <summary>
+		/// Gets the rank.
+		/// </summary>
+		/// <value>The rank.</value>
+		public uint Rank { get { return rank; } }
+
+		/// <summary>
+		/// Gets the length of each side.
+		/// </summary>
+		/// <value><see cref="Rank"/>^2</value>
+		public uint Length { get { return rank * rank; } }
+
+		/// <summary>
+		/// Gets the total number of cells.
+		/// </summary>
+		/// <value><see cref="Length"/>^2</value>
+		public uint Count { get { return Length * Length; } }
 	}
 }
--- Tests/Sudoku.cs
+++ Tests/Sudoku.cs
@@ -4,13 +4,35 @@ using System;
 
 namespace SudokuTests
 {
-	[TestFixture ()]
+	[TestFixture]
 	public class Sudoku
 	{
-		[Test ()]
+		[Test]
 		public void CreateInstance ()
 		{
-			SudokuSolver.Sudoku sudoku = new SudokuSolver.Sudoku ();
+			SudokuSolver.Sudoku sudoku = new SudokuSolver.Sudoku (3U);
+			Assert.IsNotNull (sudoku);
+		}
+
+		[Test]
+		public void CheckRank ()
+		{
+			SudokuSolver.Sudoku sudoku = new SudokuSolver.Sudoku (3U);
+			Assert.AreEqual (3U, sudoku.Rank);
+		}
+
+		[Test]
+		public void CheckLength ()
+		{
+			SudokuSolver.Sudoku sudoku = new SudokuSolver.Sudoku (3U);
+			Assert.AreEqual (9U, sudoku.Length);
+		}
+
+		[Test]
+		public void CheckCount ()
+		{
+			SudokuSolver.Sudoku sudoku = new SudokuSolver.Sudoku (3U);
+			Assert.AreEqual (81U, sudoku.Count);
 		}
 	}
 }
