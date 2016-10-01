add8b6b7ec152b6f9c0012505fcaa549234c97d9
Baljit Singh <basingh89@gmail.com>
2016-09-30 23:22:30 -0400

    Namespace and project setup

---
+++ Sudoku/Sudoku.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SudokuSolver
+{
+	public class Sudoku
+	{
+		
+	}
+}
---
+++ Tests/Sudoku.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+
+namespace SudokuTests
+{
+	[TestFixture ()]
+	public class Sudoku
+	{
+		[Test ()]
+		public void CreateInstance ()
+		{
+			SudokuSolver.Sudoku sudoku = new SudokuSolver.Sudoku ();
+		}
+	}
+}
